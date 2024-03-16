7fa2d5d8cd29b949c9c3103ec99ab47e300a2190
AbraMur <abramovu073@gmail.com>
2024-03-16 19:39:09 +0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,85 @@
+import pygame
+import config
+from grid import Grid
+from tank import Tank
+from rect import Rect
+from bullet import Bullet
+
+
+# основной класс в котором происходят все события
+class App(object):
+    def __init__(self):  # метод инициализации
+        self.screen = pygame.display.set_mode((config.WIN_WIDTH, config.WIN_HEIGHT))  # чтобы окошечко было
+        self.running = True  # переменная что бы работал главный цикл
+        self.clock = pygame.time.Clock()
+
+        self.objects = []  # набор объектов
+
+        self.grid = Grid()  # переменная класса сетки
+        self.objects.append(self.grid)  # добавляет объект сетка в список
+        self.grid.create_perlin_map()
+        self.grid_dict = self.grid.get()
+
+        self.rect = Rect()  # переменная класса rect
+
+        self.tank = Tank(self, (100, 100))  # экземпляр танка
+        self.tank_box = self.rect.collision_pos((100, 100), config.tank_width, config.tank_height)  # получаем точки танка
+        self.objects.append(self.tank)  # добавляет объект танк в список
+
+        self.bullets = []  # список выпущенных снарядов
+
+    def update(self, dt):  # метод
+        for obj in self.objects:
+            obj.update(dt)
+        for bull in self.bullets:
+            bull.fly(dt)
+
+    def event_handler(self, event):  # обработчик событий
+        if event.type == pygame.QUIT:
+            self.stop()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                self.stop()
+            if event.key == pygame.K_g:
+                self.bullets.append(Bullet((self.tank.x, self.tank.y), self, self.tank.rotate, self.tank))
+
+    def input(self):  # метод, который проверяет нажатие клавиш
+        for obj in self.objects:
+            obj.input(pygame.key.get_pressed())
+
+    def run(self):  # метод, который отвечает за бесконечный цикл программы
+        while self.running:
+            for event in pygame.event.get():
+                self.event_handler(event)
+
+            dt = self.clock.tick(config.FPS)/1000  # вычисляем delta time
+
+            self.input()
+            self.border_map()
+            self.update(dt)
+            self.draw()
+
+    def border_map(self):  # метод, в котором проверяется выход снаряда за карту
+        for bull in self.bullets:
+            if bull.x > 150 or bull.x < 0:
+                self.bullets.pop(self.bullets.index(bull))
+            if bull.y > 150 or bull.y < 0:
+                self.bullets.pop(self.bullets.index(bull))
+
+    def draw(self):  # метод отрисовки
+        self.screen.fill(config.WHITE)
+
+        for obj in self.objects:
+            obj.draw(self.screen)
+        pygame.display.update()
+
+        for bull in self.bullets:
+            bull.draw(self.screen)
+
+    def stop(self):  # останавливаем работу кода
+        self.running = False
+
+
+if __name__ == '__main__':
+    game = App()
+    game.run()
---
+++ grid.py
@@ -0,0 +1,77 @@
+import random
+import pygame
+from config import *
+from numpy import floor
+from perlin_noise import PerlinNoise
+
+
+# класс сетки
+class Grid(object):
+    ''' ВАЖНО ПОНИМАТЬ, ЧТО СЕТКА ИМЕЕТ ВИД dict[(local_x, local_y)] = ((global_x, global_y), height)
+    key = (local_x, local_y) values = ((global_x, global_y), height) '''
+
+    def __init__(self):  # создаем переменную "сетки", указываем её тип (словарь)
+        self.grid = dict()
+
+        self.heights = []  # набор высот
+
+    def update(self, dt):  # ???
+        pass
+
+    def input(self, key_pressed):  # ???
+        pass
+
+    def draw(self, screen):  # метод отрисовки сетки
+        for i in range(numbers_width_grid + 1):
+            for j in range(numbers_height_grid + 1):
+                inf = self.grid[(i, j)]
+                color = (abs(inf[1] * 100), abs(inf[1] * 100), abs(inf[1] * 100))
+                if inf[1]:
+                    pygame.draw.circle(screen, color, inf[0], 3)
+                    rect = pygame.Rect(inf[0][0] - 3, inf[0][1] + 3, cell_sise, cell_sise)
+                    pygame.draw.rect(screen, color, rect)
+
+    def completion(self, heights: list):  # генерируем сетку, где ключ у нас локальные координаты,
+        # а значения это глобальные координаты и высота (переводим локальные координаты в глобальные + получаем высоты)
+        for i in range(numbers_width_grid + 1):
+            for j in range(numbers_height_grid + 1):
+                self.grid[(i, j)] = (x_global_coord_grid + width * i / numbers_width_grid - width / (
+                            numbers_width_grid * 2),
+                                     y_global_coord_grid + height * j / numbers_height_grid - height / (
+                                                 numbers_height_grid * 2)), \
+                                    heights[i][j]
+
+    def globloc(self):
+        for i in range(width + 1):
+            for j in range(height + 1):
+                # self.grid[(i, j)] =
+                pass
+
+    def generate_perlin_noise(self, seed):  # метод генерации высот
+        # генерация основного шума и параметризация
+        noise = PerlinNoise(octaves=2, seed=seed)  # 4522
+        elevation = 1  # высота
+        chunk_size = 24  # размер чанка
+        terrain_width = max(numbers_width_grid, numbers_height_grid) + 1  # размер карты
+
+        # генерация матрицы для представления ландшафта
+        landscale = [[0 for i in range(terrain_width)] for i in range(terrain_width)]
+
+        for position in range(terrain_width ** 2):
+            # вычисление высоты y в координатах (x, z)
+            x = floor(position / terrain_width)
+            z = floor(position % terrain_width)
+            y = floor(noise([x / chunk_size, z / chunk_size]) * elevation)
+            landscale[int(x)][int(z)] = int(y)
+
+        altitude_L = list(landscale)  # высота
+
+        return altitude_L
+
+    def create_perlin_map(self):
+        seed = random.randint(1000, 3000)
+        heights = self.generate_perlin_noise(seed)
+        self.completion(heights)
+
+    def get(self):  # получаем сетку по запросу
+        return self.grid
